63246ebf68b6ea74030aa2ce739d609ee3a13c44
Amogh Chavan <amogh.chavan@E2E-DTP-028.local>
2021-10-25 14:31:07 +0530

    ip change to 0.0.0.0

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import {
-    FastifyAdapter,
-    NestFastifyApplication,
+  FastifyAdapter,
+  NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import fastifyMultipart from 'fastify-multipart';
 async function bootstrap() {
-    const app = await NestFactory.create<NestFastifyApplication>(
-        AppModule,
-        new FastifyAdapter({ logger: true })
-    );
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ logger: true }),
+  );
 
-    app.register(fastifyMultipart);
-    app.enableCors();
-    const config = new DocumentBuilder()
-        .setTitle('File uploader')
-        .setDescription('')
-        .setVersion('1.0')
+  app.register(fastifyMultipart);
+  app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('File uploader')
+    .setDescription('')
+    .setVersion('1.0')
 
-        // .addTag('Uploader')
-        .build();
+    // .addTag('Uploader')
+    .build();
 
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
-
-    await app.listen(3000, '192.168.29.248');
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
